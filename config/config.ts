6ad19b046bb090c77b840999cbec7160aa6b6549
Gordon Wong <huang_chuojun@126.com>
2024-05-10 16:43:58 +0800

    完善登录，注销接口

--- config/config.ts
+++ config/config.ts
@@ -129,15 +129,10 @@ export default defineConfig({
   openAPI: [
     {
       requestLibPath: "import { request } from '@umijs/max'",
-      // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-      schemaPath: join(__dirname, 'oneapi.json'),
-      mock: false,
-    },
-    {
-      requestLibPath: "import { request } from '@umijs/max'",
-      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-      projectName: 'swagger',
+      // 在线的json文档地址
+      schemaPath: 'http://localhost:7530/api/v3/api-docs',
+      // 后端项目名称
+      projectName: 'gordonapi-backend',
     },
   ],
   mfsu: {
